b13b3304b36de54052f64aa08ff7f27a7e451b88
ptx-pluto <sheep9275@gmail.com>
2015-01-26 03:15:37 +0800

    thumbnail in list added

--- src/app/models/Link.js
+++ src/app/models/Link.js
@@ -2,22 +2,28 @@
 
 module.exports = Ember.Object.extend({
 
-    isImage: function(){
+    isImage: function () {
         return this.get('hasFull') || this.get('hasThumbnail');
     }.property('hasThumbnail', 'hasFull'),
 
-    hasThumbnail: function(){
+    hasThumbnail: function () {
         var thumb = this.get('thumbnail');
         return !!thumb && thumb != 'self' && thumb != 'default' && thumb != 'nsfw';
     }.property('thumbnail'),
 
-    hasFull: function(){
+    hasFull: function () {
         var url = this.get('url');
         return url && url.match(/.(jpg)|(jpeg)|(gif)|(png)$/);
     }.property('url'),
 
-    redditLink: function(){
+    redditLink: function () {
         return 'https://www.reddit.com' + this.get('permalink');
-    }.property('permalink')
+    }.property('permalink'),
+
+    backgroundStyle: function () {
+        if (this.get('hasThumbnail')) {
+            return 'background-image: url(' + this.get('thumbnail') + ')';
+        }
+    }.property('hasThumbnail', 'thumbnail')
 
 });
