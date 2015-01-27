6b2b64a33c84f5600d849720ae21448dfd33c68e
ptx-pluto <sheep9275@gmail.com>
2015-01-27 19:47:13 +0800

    better feed list

--- src/app/controllers/FeedController.js
+++ src/app/controllers/FeedController.js
@@ -2,5 +2,12 @@
 
 module.exports = Ember.ObjectController.extend({
 
+    needs: ['subredditPost'],
+
+    current: Ember.computed.alias('controllers.subredditPost.post.id'),
+
+    isCurrent: function(){
+        return this.get('id') === this.get('current');
+    }.property('id', 'current')
 
 });
