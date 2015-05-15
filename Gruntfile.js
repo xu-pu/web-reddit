c64b33f9b140eef35b329445f66f9be72e274795
ptx-pluto <sheep9275@gmail.com>
2015-05-15 23:21:07 +0800

    dist added

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,6 +65,23 @@ module.exports = function (grunt) {
                 }
             }
 
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            dist: {
+                files: {
+                    'dist/application.min.js': [
+                        'venders/jquery/dist/jquery.js',
+                        'venders/ember/ember.min.js',
+                        'venders/wookmark-jquery/jquery.wookmark.min.js',
+                        'build/scripts/templates.js',
+                        'build/scripts/application.js'
+                    ]
+                }
+            }
         }
 
     });
@@ -74,11 +91,17 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-ember-templates');
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('build', [
         'browserify:build',
         'emberTemplates:build',
         'compass:build'
+    ]);
+
+    grunt.registerTask('dist', [
+        'build',
+        'uglify:dist'
     ]);
 
 };
